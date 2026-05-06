879af8a4355b42158836453828ed9fd824372d6f
peralta.dan1208 <peralta.dan1208@gmail.com>
2026-05-06 18:06:34 +0800

    replaced axios baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: 'https://blogwebsite-api-gkq5.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
